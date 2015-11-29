45b157489725e9c1df9b15e298216932dc2e96f2
Corretx <anna.corretger@barcelonagse.eu>
2015-11-29 11:34:13 +0100

    Update data_and_analysis.php

--- web/data_and_analysis.php
+++ web/data_and_analysis.php
@@ -50,6 +50,24 @@
     query_and_print_graph($query,$title,"People");
 ?>
 
+<?php
+    // Total Revenue by product
+    
+    $query = "Select age,gender from Group9db.Age_Gender";
+    $query2 = "Select age,gender from Group9db.Age_Gender";
+    $title = "Distribution by age";
+    query_and_print_graph_multibar1($query,$query2,$title,"People");
+?>
+
+<?php
+    // Total Revenue by product
+    
+    $query = "Select age,gender from Group9db.Age_Gender";
+    $query2 = "Select age,gender from Group9db.Age_Gender";
+    $title = "Distribution by age";
+    query_and_print_graph_multibar2($query,$query2,$title,"People");
+?>
+
 
 	<p>The chart below shows the results of a similar analysis, this time to rank the customers that contribute the most to total revenues. Only the top 20 customers are shown below.</p>
 	
