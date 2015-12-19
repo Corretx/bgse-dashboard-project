f63f75082bfff17d96ab8e8e104442493e60ec73
Corretx <anna.corretger@barcelonagse.eu>
2015-12-19 16:58:37 +0100

    Update analysis.php

--- web/analysis.php
+++ web/analysis.php
@@ -42,8 +42,8 @@
     </form>
 
  <?php
-   $rEngine = "/usr/local/bin/Rscript --vanilla ";
-   $rScript = "~/Documents/MyApp/analysis/Clustering.R";
+   $rEngine = "/usr/bin/Rscript --vanilla ";
+   $rScript = "/home/ubuntu/projects/bgse-dashboard-project-group9/analysis/Clustering.R";
 
    $cmd = sprintf("%s %s", $rEngine, $rScript); #  >&1 2>&1
    $result = system($cmd);
