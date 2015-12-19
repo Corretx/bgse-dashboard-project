5280417dc7e95a0ae84a41f4f89bb9f3983bac2b
Corretx <anna.corretger@barcelonagse.eu>
2015-12-19 17:44:24 +0100

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,7 +1,8 @@
 # Install R packages required
 
-if("RMySQL" %in% rownames(installed.packages()) == FALSE) {install.packages("RMySQL", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
-if("reshape2" %in% rownames(installed.packages()) == FALSE) {install.packages("reshape2", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
-if("plyr" %in% rownames(installed.packages()) == FALSE) {install.packages("plyr", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
-if("glmnet" %in% rownames(installed.packages()) == FALSE) {install.packages("glmnet", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
+install.packages("RMySQL", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("reshape2", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("plyr", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("glmnet", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("DBI", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
 
