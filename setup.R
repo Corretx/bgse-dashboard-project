cf7c37106e7a9cdc3225952c76461df0e4459628
Corretx <anna.corretger@barcelonagse.eu>
2015-12-19 16:55:39 +0100

    Create setup.R

---
+++ setup.R
@@ -0,0 +1,6 @@
+# Install R packages required
+
+install.packages("reshape2", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("plyr", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("RMySQL", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("glmnet", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
