77c1ff950a8aeb0374e2d4fa11513894b539f1a4
Corretx <anna.corretger@barcelonagse.eu>
2015-12-19 17:36:31 +0100

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,6 +1,7 @@
 # Install R packages required
 
-install.packages("reshape2", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
-install.packages("plyr", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
-install.packages("RMySQL", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
-install.packages("glmnet", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+if("RMySQL" %in% rownames(installed.packages()) == FALSE) {install.packages("RMySQL", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
+if("reshape2" %in% rownames(installed.packages()) == FALSE) {install.packages("reshape2", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
+if("plyr" %in% rownames(installed.packages()) == FALSE) {install.packages("plyr", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
+if("glmnet" %in% rownames(installed.packages()) == FALSE) {install.packages("glmnet", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")}
+
