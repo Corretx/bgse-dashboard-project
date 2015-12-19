475c3493e88c3791952eff6e7d34933d895dbea4
Santhosh Narayanan <nsanthosh27@gmail.com>
2015-12-19 17:59:23 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -11,7 +11,17 @@
 </div>
 
 <div id="main">
-
-    Home
+<h3>Comparative Effectiveness Research:</h3>
+<p>Designed to inform health-care decisions by providing evidence on the effectiveness, benefits, and harms of different treatment options. For our analysis we compare two different treatment options for diabetes patients and
+study their outcomes.</p>
+<ol>We follow the following steps perform this analysis.
+  <li>Identify our population of interest.</li>
+  <li>Separate into two cohorts based on the drugs of interest.</li>
+  <li>Develop a logistic regression model to calculate propensity scores for treatment.</li>
+  <li>Perform balancing of cohorts using propensity score matching.</li>
+  <li>Rank variables in terms of their importance to the outcome using a lasso model.</li>
+  <li>Create an association matrix to identify the clusters that show the highest statistically
+significant differences in the outcome.</li>
+</ol>
 
 </div>
